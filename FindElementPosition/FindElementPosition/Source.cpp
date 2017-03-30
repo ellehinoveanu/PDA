1f4af277f658c0643c1b1738b10d9510042f7b8e
Elle <ellehinoveanu@gmail.com>
2017-03-30 11:52:06 +0300

    Changes

--- FindElementPosition/FindElementPosition/Source.cpp
+++ FindElementPosition/FindElementPosition/Source.cpp
@@ -1,19 +1,27 @@
 #include "mpi.h"
 #include<iostream>
 #include<cstdlib>
+#include<ctime>
 
-#define SIZE 6
+#define SIZE 200
 #define MASTER 0
 #define SEARCHEDVALUE 11
 #define NUMPROCESSES  7
 
 int main(int argc, char *argv[])
 {
-	int vector[SIZE] = { 2, 3, 10, 11, 11, 11 },  searchedValue , start, nextStep, counter, processId, numberOfProcesses ;
+	int vector[SIZE] /*= { 2, 3, 10, 11, 11, 11 }*/,  searchedValue , start, nextStep, counter, processId, numberOfProcesses ;
 	searchedValue = SEARCHEDVALUE;
 	numberOfProcesses = NUMPROCESSES;
-	
-	
+	srand(time(NULL));
+
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		vector[i] = rand() % 100;
+	}
+
+
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &numberOfProcesses);
 	MPI_Comm_rank(MPI_COMM_WORLD, &processId);
@@ -30,7 +38,7 @@ int main(int argc, char *argv[])
 		{
 			printf("Number found at position : %d \n", i);
 		}
-		else printf("Not found");
+		else printf("Not found\n");
 	}
 	MPI_Finalize();
 	
